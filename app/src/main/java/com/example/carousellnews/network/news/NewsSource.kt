ec2db18dc9ae5c649a756859027630f286adf6e4
Sonali Sharma <sosharma@falabella.cl>
2022-08-31 15:21:27 +0530

    remove unused imports

--- app/src/main/java/com/example/carousellnews/network/news/NewsSource.kt
+++ app/src/main/java/com/example/carousellnews/network/news/NewsSource.kt
@@ -1,9 +1,7 @@
 package com.example.carousellnews.network.news
 
-import androidx.lifecycle.MutableLiveData
 import com.example.carousellnews.network.data.NewsApi
 import com.example.carousellnews.network.model.NewsItem
-import java.util.*
 import javax.inject.Inject
 
 class NewsSource @Inject constructor(private val api: NewsApi) {
